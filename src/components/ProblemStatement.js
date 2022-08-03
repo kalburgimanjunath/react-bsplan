ef90bd644bb628b1ef7d536592effa8eaae05582
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2022-08-03 20:32:54 +0530

    2 files modified, 2 added

--- src/components/ProblemStatement.js
+++ src/components/ProblemStatement.js
@@ -3,9 +3,44 @@ import { Formik, Form, Field, ErrorMessage } from 'formik';
 import axios from 'axios';
 
 const postData = async (ProblemStatement) => {
-  let headers = new Headers();
-  headers.append('Authorization', 'Bearer keyeNXyxxuuYJY19w');
-  headers.append('Content-Type', 'application/json');
+  const base = require('airtable').base('appE590l4XAEEfNUQ');
+  var Airtable = require('airtable');
+  Airtable.configure({
+    endpointUrl: 'https://api.airtable.com',
+    apiKey: 'keyeNXyxxuuYJY19w',
+  });
+  var base = Airtable.base('appE590l4XAEEfNUQ');
+
+  base('PlanTable')
+    .select({
+      // Selecting the first 3 records in Grid view:
+      maxRecords: 3,
+      view: 'Grid view',
+    })
+    .eachPage(
+      function page(records, fetchNextPage) {
+        // This function (`page`) will get called for each page of records.
+
+        records.forEach(function (record) {
+          console.log('Retrieved', record.get('ProblemStatement'));
+        });
+
+        // To fetch the next page of records, call `fetchNextPage`.
+        // If there are more records, `page` will get called again.
+        // If there are no more records, `done` will get called.
+        fetchNextPage();
+      },
+      function done(err) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+      }
+    );
+
+  // let headers = new Headers();
+  // headers.append('Authorization', 'Bearer keyeNXyxxuuYJY19w');
+  // headers.append('Content-Type', 'application/json');
   // let res = await fetch(
   //   'https://api.airtable.com/v0/appE590l4XAEEfNUQ/PlanTable',
   //   {
@@ -16,22 +51,22 @@ const postData = async (ProblemStatement) => {
   //     }),
   //   }
   // );
-  axios
-    .post(
-      'https://api.airtable.com/v0/appE590l4XAEEfNUQ/PlanTable',
-      {
-        fields: {
-          ProblemStatement: ProblemStatement,
-        },
-      },
-      { headers: headers }
-    )
-    .then((resp) => {
-      console.log('success!');
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+  //   axios
+  //     .post(
+  //       'https://api.airtable.com/v0/appE590l4XAEEfNUQ/PlanTable',
+  //       {
+  //         fields: {
+  //           ProblemStatement: ProblemStatement,
+  //         },
+  //       },
+  //       { headers: headers }
+  //     )
+  //     .then((resp) => {
+  //       console.log('success!');
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error);
+  //     });
 };
 export default function ProblemStatement({ ProblemStatement }) {
   return (
@@ -48,7 +83,7 @@ export default function ProblemStatement({ ProblemStatement }) {
         }}
         onSubmit={(values, { setSubmitting }) => {
           setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
+            // alert(JSON.stringify(values, null, 2));
             postData(values);
             setSubmitting(false);
           }, 400);
